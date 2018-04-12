06183510f9e8359899a250fa9b805e73dea3d1fa
madrury <matthew.drury.83@gmail.com>
2018-04-12 08:32:21 -0700

    Add etc/config.py

---
+++ etc/config.py
@@ -0,0 +1,39 @@
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+
+FLOOR_CONFIG = {
+    'width': 80,
+    'height': 43,
+    'max_rooms': 50,
+}
+
+ROOM_CONFIG = {
+    'width': 10,
+    'height': 10,
+    'max_rectangles': 5,
+    'max_rectangle_width': 3,
+    'max_rectangle_height': 3,
+}
+
+MAP_CONFIG = {
+    'max_monsters_per_room': 3,
+    'max_items_per_room': 2
+}
+
+PANEL_CONFIG = {
+    'bar_width': 20,
+    'height': 7,
+}
+PANEL_CONFIG['y'] = SCREEN_HEIGHT - PANEL_CONFIG['height']
+
+MESSAGE_CONFIG = {
+    'x': PANEL_CONFIG['bar_width'] + 2,
+    'width': SCREEN_WIDTH - PANEL_CONFIG['bar_width'] - 2,
+    'height': PANEL_CONFIG['height'] - 1
+}
+
+FOV_CONFIG = {
+    "algorithm": 'BASIC',
+    "light_walls": True,
+    "radius": 10
+}
--- roguelike.py
+++ roguelike.py
@@ -1,6 +1,9 @@
 import tdl
 
 from etc.colors import COLORS
+from etc.config import (
+   SCREEN_WIDTH, SCREEN_HEIGHT, FLOOR_CONFIG, ROOM_CONFIG,
+   MAP_CONFIG, PANEL_CONFIG, MESSAGE_CONFIG, FOV_CONFIG)
 
 from components.attacker import Attacker
 from components.harmable import Harmable
@@ -23,46 +26,6 @@ from render_functions import (
 
 
 def main():
-
-    SCREEN_WIDTH = 80
-    SCREEN_HEIGHT = 50
-
-    FLOOR_CONFIG = {
-        'width': 80,
-        'height': 43,
-        'max_rooms': 50,
-    }
-
-    ROOM_CONFIG = {
-        'width': 10,
-        'height': 10,
-        'max_rectangles': 5,
-        'max_rectangle_width': 3,
-        'max_rectangle_height': 3,
-    }
-
-    MAP_CONFIG = {
-        'max_monsters_per_room': 3,
-        'max_items_per_room': 2
-    }
-
-    PANEL_CONFIG = {
-        'bar_width': 20,
-        'height': 7,
-    }
-    PANEL_CONFIG['y'] = SCREEN_HEIGHT - PANEL_CONFIG['height']
-
-    MESSAGE_CONFIG = {
-        'x': PANEL_CONFIG['bar_width'] + 2,
-        'width': SCREEN_WIDTH - PANEL_CONFIG['bar_width'] - 2,
-        'height': PANEL_CONFIG['height'] - 1
-    }
-
-    FOV_CONFIG = {
-        "algorithm": 'BASIC',
-        "light_walls": True,
-        "radius": 10
-    }
 
     tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
 
