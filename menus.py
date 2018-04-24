06a078898daa0dc9f44913a655048c0362dd367e
madrury <matthew.drury.83@gmail.com>
2018-04-23 20:42:17 -0700

    Improve look of menus.

--- menus.py
+++ menus.py
@@ -2,12 +2,14 @@ import tdl
 import textwrap
 import string
 
-def menu(header, options, width, screen_width, screen_height):
+from etc.colors import COLORS
+
+def menu(header, options, width, screen_width, screen_height,
+         header_buffer=1, border_buffer=1):
     """Draw a generic menu with a header and options for selection.
 
     Arguments
     ---------
-    
     header: str
       A text string to print at the top of a menu for description.
 
@@ -22,20 +24,27 @@ def menu(header, options, width, screen_width, screen_height):
 
     screen_height: int
       The height of the screen into which the menu will be drawn.
+
+    Returns
+    -------
+    (window, x_position, y_position): tdl.Console, int, int:
+      The console contining the window, and the position to blit the console
+      onto the main console.
     """
     if len(options) > 26:
         raise ValueError('Cannot have a menu with more than 26 options.')
     header_wrapped = textwrap.wrap(header, width)
     header_height = len(header_wrapped)
-    height = len(options) + header_height
+    height = len(options) + header_height + header_height + 4*border_buffer
 
     window = tdl.Console(width, height)
-    window.draw_rect(0, 0, width, height, None, fg=(255, 255, 255), bg=None)
+    window.draw_rect(0, 0, width, height, None, fg=COLORS['white'], bg=None)
+    window.draw_frame(0, 0, width, height, '~', fg=None, bg=COLORS['darker_red'])
     for i, line in enumerate(header_wrapped):
-        window.draw_str(0, i, header_wrapped[i])
-    for i, (y, option) in enumerate(enumerate(options, start=header_height)):
+        window.draw_str(2, i+2, header_wrapped[i])
+    for i, (y, option) in enumerate(enumerate(options, start=header_height + 3)):
         text = '(' + string.ascii_lowercase[i] + ') ' + option
-        window.draw_str(0, y, text)
+        window.draw_str(2, y, text)
     # Return the position to blit the new console
     return window, screen_width // 2 - width // 2, screen_height //2 - height // 2
 
