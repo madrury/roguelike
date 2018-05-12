37c4808c401a7980aa752d976d3d960530285188
madrury <matthew.drury.83@gmail.com>
2018-05-11 20:09:30 -0700

    Spawnable parent class -> spawn_in_room

--- spawnable/spawnable.py
+++ spawnable/spawnable.py
@@ -10,28 +10,27 @@ def spawn_entities(schedule, group_definitions, game_map, entities):
 
 def spawn_group(group_definition, room, game_map, entities):
     for entity_type in group_definition:
-        entity = entity_type.spawn(room, game_map, entities)
+        entity = spawn_in_room(entity_type, room, game_map, entities)
         if entity is not None:
             entities.append(entity)
+
+def spawn_in_room(entity_type, room, game_map, entities, max_tries=25):
+    for _ in range(max_tries):
+        x, y = room.random_point()
+        if not any((x, y) == (entity.x, entity.y) for entity in entities):
+            entity = entity_type.make(x, y)
+            break
+    else:
+        entity = None
+    if entity and entity_can_spawn_in_space(entity, x, y, game_map):
+        game_map.blocked[x, y] = True
+        return entity
+    else:
+        return None
 
 def entity_can_spawn_in_space(entity, x, y, game_map):
     return (entity.swims or not game_map.water[x, y])
 
 
 class Spawnable:
-
-    @classmethod
-    def spawn(cls, room, game_map, entities, max_tries=25):
-        for _ in range(max_tries):
-            x, y = room.random_point()
-            if not any((x, y) == (entity.x, entity.y) for entity in entities):
-                entity = cls.make(x, y)
-                break
-        else:
-            entity = None
-        if entity and entity_can_spawn_in_space(entity, x, y, game_map):
-            game_map.blocked[x, y] = True
-            return entity
-        else:
-            return None
-
+    pass
